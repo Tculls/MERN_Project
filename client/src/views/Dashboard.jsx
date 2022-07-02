6d5e7e6b3731d5f374b539015031cc33c80e4398
Tculls <tanner.culley@gmail.com>
2022-07-02 09:51:59 -0700

    adding login and reg / figuring out apis

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,76 +1,63 @@
 import React from 'react';
-import {useEffect, useState, useParams} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
-import TwelveData from "twelvedata";
 
 
 
 const Dashboard = () => {
-    const TwelveData = require("twelvedata")
     const [stocks, setStocks] = useState([])
-    const config = {
-        key: "bef906f480804f6499671ac7acc4854e"
-    };
-    const client = TwelveData(config);
-    let params = {
-        symbol: "AAPL",
-        interval: "1min",
-        outputsize: 5,
-    };
-    client
-    .timeSeries(params)
-    .then((data) =>{
 
-    })
-    .catch((error) =>{
+    // useEffect(() => {
+    //     axios.get(`https://api.stockdata.org/v1/data/quote?symbols=AAPL%2CTSLA%2CMSFT&api_token=GOA2gwM2LCoC6JYQdpsMqVD8saUaaFmruAxMfN5c`)
+    //     .then(response=>{
+    //         console.log(response.data)
+    //         setStocks(response.data)
+    //     })
+    //     .catch(err=> console.log(err))
+    // }, [])
 
-    });
-    useEffect(() => {
-        axios.get(`https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=bef906f480804f6499671ac7acc4854e`)
-        .then(response=>{
-            console.log(response.data)
-            setStocks(response.data)
-        })
-        .catch(err=> console.log(err))
-    })
-
-    return (
-        <div className="App">
-            <header className="">
-                <h1 className="title">EquityEvaluator</h1>
-                <form class="search" action="">
-                    <input type="text" placeholder="Enter Symbol or Name" name="search" />
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-                <div className="favorites">
-                    <h4>Favorite tickers</h4>
-
+        return (
+            <div className="App">
+                <header className="">
+                    <h1 className="title">EquityEvaluator</h1>
+                    <form class="search" action="">
+                        <input type="text" placeholder="Enter Symbol or Name" name="search" />
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                    <div className="favorites">
+                        <h4>Favorite tickers</h4>
+                    </div>
+                </header>
+                <h2>Indicies today</h2>
+                <div className="main">
+                    <div className="box1">
+                        <h4>Dow Jones</h4>
+                        {
+                        stocks.map((stock, i )=> (
+                            <div key={i}>
+                            <p>{stock.symbol}</p>
+                            <p>{stock.grossProfit}</p>
+                        
+                        </div>
+                        ))}
+                    </div>
+                    <div className="box2">
+                        <h4>S&P 500</h4>
+                    </div>
+                    <div className="box3">
+                        <h4>Dow Jones</h4>
+                    </div>
                 </div>
-            </header>
-            <h2>Indicies today</h2>
-            <div className="main">
-                <div className="box1">
-                    <h4>Dow Jones</h4>
-                    {
-                        stocks.map((stock, i) =>{
-                            <div key={i}/>
-                        })
-                    }
-                </div>
-                <div className="box2">
-                    <h4>S&P 500</h4>
-                </div>
-                <div className="box3">
-                    <h4>Dow Jones</h4>
-                </div>
+    <script type="module" src="./app.js"></script>
             </div>
-<script type="module" src="./app.js"></script>
-        </div>
-        
-    );
-}
+            
+        );
+    }
+    
+
 
 
 export default Dashboard
 
-//  TwelveData bef906f480804f6499671ac7acc4854e
+//  TwelveData bef906f480804f6499671ac7acc4854e API KEY 1 completely cooked for 140 days
+// # 2 285759c7ba3643eeafe2303fe3e5e88c
---
+++ server/model/user.model.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt');
+
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+      .then(hash => {
+        this.password = hash;
+        next();
+      });
+  });
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+      type: String,
+      required: [true, "First name is required"]
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"]
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      validate: {
+        validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+        message: "Please enter a valid email"
+      }
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be 8 characters or longer"]
+      
+    }
+  }, {timestamps: true});
+
+  UserSchema.virtual('confirmPassword')
+    .get( () => this._confirmPassword) 
+    .set(value => this._confirmPassword = value);
+
+  UserSchema.pre('validate', function(next) {
+    if (this.password !== this._confirmPassword) {
+        this.invalidate('confirmPassword', 'Password must match')
+    }
+    next();
+  });
